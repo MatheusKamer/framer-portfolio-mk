8935d50a108f7d436d39f48fd8555d3de72e9c5a
Matheus Kamer <matheus_kamer@hotmail.com>
2025-11-08 16:14:48 -0400

    feat: enhance Skills component with animations and motion effects

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -4,15 +4,33 @@ import Image from "next/image";
 import Heading from "./sub/Heading";
 
 import { skillsData } from "@/assets";
+import { motion } from "framer-motion";
 
 const Skills = () => {
+  const variants = {
+    visible: (index: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.3 + index * 0.1,
+      },
+    }),
+    hidden: { opacity: 0, y: 30 },
+  };
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-y-20 px-96">
       <Heading text="Skills" />
       <div className="w-full flex justify-between flex-wrap gap-x-4 gap-y-6 lg:gap-y-10">
         {skillsData.map((skill, index) => (
-          <div
+          <motion.div
             key={index}
+            custom={index}
+            variants={variants}
+            initial="hidden"
+            whileInView="visible"
+            whileHover={{ scale: 1.1 }}
+            viewport={{ margin: "50px", once: true }}
             className="flex items-center justify-center gap-x-3 rounded-xl border border-yellow-500 bg-zinc-200 px-2 lg:px-5 py-2"
           >
             <Image
@@ -23,7 +41,7 @@ const Skills = () => {
               className="h-auto w-10"
             />
             <p className="text-sm text-gray-600">{skill.name}</p>
-          </div>
+          </motion.div>
         ))}
       </div>
     </div>
