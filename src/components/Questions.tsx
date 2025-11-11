b8f1956ea4fbab86f21a6cee6893db670db62297
Matheus Kamer <matheus_kamer@hotmail.com>
2025-11-10 22:01:48 -0400

    feat: add Questions component for FAQ section and integrate into Home page

---
+++ src/components/Questions.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Heading from "./sub/Heading";
+
+import { questions } from "@/assets";
+import Question from "./sub/Question";
+
+const Questions = () => {
+  return (
+    <div className="py-20 px-96">
+      <Heading text="Questions & Answers" />
+      <div>
+        <ul className="flex flex-col gap-y-3">
+          {questions.map((question, index) => (
+            <Question
+              key={index}
+              index={index}
+              question={question.question}
+              answer={question.answer}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Questions;
---
+++ src/components/sub/Question.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+import { questionArrow } from "@/assets";
+import { motion } from "framer-motion";
+
+interface iQuestion {
+  index: number;
+  question: string;
+  answer: string;
+}
+
+const Question = ({ index, question, answer }: iQuestion) => {
+  const [showAnswer, setShowAnswer] = useState(false);
+
+  const variants = {
+    visible: (index: number) => ({
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: index * 0.2,
+      },
+    }),
+    hidden: { opacity: 0, x: -30 },
+  };
+
+  return (
+    <motion.li
+      custom={index}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ margin: "50px", once: true }}
+      variants={variants}
+      className="border border-yellow-500 p-1 rounded-lg"
+    >
+      <h1
+        className={`flex items-center text-xl text-gray-800 hover:text-yellow-600 font-extralight tracking-wide cursor-pointer ${
+          showAnswer && "border-b text-yellow-600"
+        }`}
+        onClick={() => setShowAnswer(!showAnswer)}
+      >
+        <motion.span animate={{ rotate: showAnswer ? 180 : 0 }} className="">
+          {questionArrow}
+        </motion.span>
+        <span>{question}</span>
+      </h1>
+      <motion.p
+        initial={{ scaleY: 0, height: 0, opacity: 0 }}
+        animate={{
+          scaleY: showAnswer ? 1 : 0,
+          height: showAnswer ? "auto" : 0,
+          opacity: showAnswer ? 1 : 0,
+        }}
+        transition={{
+          duration: 0.1,
+          type: "spring",
+          stiffness: showAnswer ? 250 : 50,
+          opacity: { delay: showAnswer ? 0.2 : 0 },
+        }}
+        className="pl-8 text-lg font-extralight tracking-wide text-gray-900 first-letter:pl-3"
+      >
+        {answer}
+      </motion.p>
+    </motion.li>
+  );
+};
+
+export default Question;
