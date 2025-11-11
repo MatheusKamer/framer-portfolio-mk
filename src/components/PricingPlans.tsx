2cd26d534540a8ebdba56fb132a2776119fb2750
Matheus Kamer <matheus_kamer@hotmail.com>
2025-11-10 20:10:27 -0400

    feat: enhance PricingPlans component with animations and improved structure

--- src/components/PricingPlans.tsx
+++ src/components/PricingPlans.tsx
@@ -1,6 +1,7 @@
 import Heading from "./sub/Heading";
 
 import { checkIcon, pricingPlans } from "@/assets";
+import { motion } from "framer-motion";
 
 const PricingPlans = () => {
   return (
@@ -8,9 +9,22 @@ const PricingPlans = () => {
       <Heading text="Pricing Plans" />
       <div className="h-full flex flex-col lg:flex-row items-center justify-between gap-8">
         {pricingPlans.map((plan, index) => (
-          <div
+          <motion.div
+            initial={{ opacity: 0, y: index % 2 === 0 ? -100 : 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            whileHover={{ scale: 1.05 }}
+            viewport={{ once: true }}
+            transition={{
+              duration: 0.4,
+              delay: index * 0.2,
+              scale: { duration: 0.15 },
+            }}
             key={index}
-            className="w-[270px] sm:w-[300px] xl:w-[350px] bg-zinc-50 flex flex-col gap-y-6 p-6 border border-red-400 rounded-xl text-gray-600"
+            className={`w-[270px] flex flex-col gap-y-6 p-6 border border-red-400 rounded-xl text-gray-600 ${
+              plan.title === "Premium"
+                ? "sm:w-[320px] xl:w-[370px] bg-white"
+                : "sm:w-[300px] xl:w-[350px] bg-zinc-50"
+            }`}
           >
             <h1 className="text-lg lg:text-3xl font-light tracking-wide text-center">
               {plan.title}
@@ -19,9 +33,17 @@ const PricingPlans = () => {
               {plan.pricing}
             </span>
             <ul className="flex flex-col gap-y-2">
-              {plan.features.map((feature, index) => (
-                <div key={index} className="flex items-center gap-x-3">
-                  <span className="text-2xl text-yellow-500">{checkIcon}</span>
+              {plan.features.map((feature, featureIndex) => (
+                <div key={featureIndex} className="flex items-center gap-x-3">
+                  <span
+                    className={`text-2xl ${
+                      plan.title === "Premium"
+                        ? "text-red-500"
+                        : "text-yellow-500"
+                    }`}
+                  >
+                    {checkIcon}
+                  </span>
                   <li className="text-[15px] font-light tracking-wide">
                     {feature}
                   </li>
@@ -32,7 +54,7 @@ const PricingPlans = () => {
               <span className="font-semibold">Ideal for:</span>{" "}
               {plan.recommended}
             </p>
-          </div>
+          </motion.div>
         ))}
       </div>
     </div>
